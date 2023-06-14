e3f31d1a658f2845c48766023bba59821005045b
Hamid Mirzapour <h.r.mirzapour@gmail.com>
2023-06-14 23:00:27 +0330

    Delete unused files and create robo card component

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = ({ id, name, email }) => {
+    return (
+        <div className="tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5">
+            <img src="https://robohash.org/{id}" alt="{email}" />
+            <div>
+                <h2>{name}</h2>
+                <h2>{email}</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
---
+++ src/containers/App.js
@@ -0,0 +1,15 @@
+import Card from '../components/Card';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <Card id={"1"} name={"Leanne Graham"} email={"Sincere@april.biz"} />
+      <Card id={"1"} name={"Leanne Graham"} email={"Sincere@april.biz"} />
+      <Card id={"1"} name={"Leanne Graham"} email={"Sincere@april.biz"} />
+      <Card id={"1"} name={"Leanne Graham"} email={"Sincere@april.biz"} />
+    </div>
+  );
+}
+
+export default App;
